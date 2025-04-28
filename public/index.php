75483926cb324212cc3a263bead3321a320e1556
fsaleseng <147283902+fsaleseng@users.noreply.github.com>
2025-04-28 17:16:34 -0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/assets/css/style-index.css">
+    <title>Portal PCM CCR</title>
+</head>
+<body>
+<main class="home-wrapper">
+    <div class="glass-card">
+        <img src="assets/img/ccr_metro_bahia.png" alt="">
+        <h1>Portal de Planejamento e Controle da Manutenção</h1><br>
+        <p class="lead">
+            Inteligência aplicada à manutenção metroviária.<br>
+            Explore cenários, dados e estratégias com tecnologia.<br><br>
+        </p>
+        <a href="public/login.php" class="btn-entrar">Acessar</a>
+    </div>
+</main>
+</body>
+</html>
